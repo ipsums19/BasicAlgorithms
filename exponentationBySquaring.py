7174f8b5bb257f8f1e607abf07cbb2064ed52814
Joan Barba <joanbarba19@gmail.com>
2016-10-22 21:41:40 +0200

    exponentation by squaring recursive

--- exponentationBySquaring.py
+++ exponentationBySquaring.py
@@ -1,4 +1,4 @@
-def exp(n, e):
+def exp2(n, e):
     if e == 0: return 1
     aux = 1
     while e > 1:
@@ -10,6 +10,11 @@ def exp(n, e):
         e /=2
     return n * aux
 
+def exp(n, e):
+    if e == 0: return 1
+    elif e%2==1: return n*exp(n*n, e/2)
+    return exp(n*n, e/2)
+
 assert exp(5,1)==5
 assert exp(5,2)==25
 assert exp(5,3)==125
@@ -18,3 +23,12 @@ assert exp(5,5)==3125
 assert exp(5,6)==15625
 assert exp(5,7)==78125
 assert exp(5,8)==390625
+
+assert exp2(5,1)==5
+assert exp2(5,2)==25
+assert exp2(5,3)==125
+assert exp2(5,4)==625
+assert exp2(5,5)==3125
+assert exp2(5,6)==15625
+assert exp2(5,7)==78125
+assert exp2(5,8)==390625
