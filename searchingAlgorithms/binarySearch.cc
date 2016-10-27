dc70861ac8ca33c6eb6fb66b5ffc33dbae027009
Joan Barba <joanbarba19@gmail.com>
2016-10-27 12:31:36 +0200

    searchingAlgorithms

--- searchingAlgorithms/binarySearch.cc
+++ searchingAlgorithms/binarySearch.cc
@@ -15,17 +15,17 @@ bool binarySearch(int search, std::vector<int> v)
     return auxBinary(search, v, 0, v.size()-1);
 }
 
-int main()
-{
-    int n;
-    std::cin >> n;
-    std::vector<int> v(n);
-    for(int i = 0; i < n; ++i)
-        std::cin >> v[i];
-    mergeSort(v, 0, n-1);
-    int search;
-    while(std::cin >> search)
-    {
-        std::cout << search <<" " <<binarySearch(search, v) << std::endl;
-    }
-}
+//int main()
+//{
+    //int n;
+    //std::cin >> n;
+    //std::vector<int> v(n);
+    //for(int i = 0; i < n; ++i)
+        //std::cin >> v[i];
+    //mergeSort(v, 0, n-1);
+    //int search;
+    //while(std::cin >> search)
+    //{
+        //std::cout << search <<" " <<binarySearch(search, v) << std::endl;
+    //}
+//}
